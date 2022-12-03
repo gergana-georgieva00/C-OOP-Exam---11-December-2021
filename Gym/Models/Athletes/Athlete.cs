5e02fd77ae282e8d7c3cd7635e4b2d0e997481c6
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-03 16:05:42 +0200

    C# OOP Exam - 11 December 2021

---
+++ Gym/Models/Athletes/Athlete.cs
@@ -0,0 +1,69 @@
+﻿using Gym.Models.Athletes.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Athletes
+{
+    public abstract class Athlete : IAthlete
+    {
+        private string fullName;
+        private string motivation;
+        private int numberOfMedals;
+
+        public Athlete(string fullName, string motivation, int numberOfMedals, int stamina)
+        {
+            this.FullName = fullName;
+            this.Motivation = motivation;
+            this.NumberOfMedals = numberOfMedals;
+            this.Stamina = stamina;
+        }
+
+        public string FullName 
+        {
+            get => this.fullName; 
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Athlete name cannot be null or empty.");
+                }
+
+                this.fullName = value;
+            }
+        }
+
+        public string Motivation
+        {
+            get => this.motivation;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("The motivation cannot be null or empty.");
+                }
+
+                this.motivation = value;
+            }
+        }
+
+        public int Stamina { get; private set; }
+
+        public int NumberOfMedals 
+        {
+            get => this.numberOfMedals;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Athlete's number of medals cannot be below 0.");
+                }
+            }
+        }
+
+        public void Exercise()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
