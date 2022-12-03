b6a73ab729336f880a8d7549d41d3da1310af40b
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-03 16:06:58 +0200

    C# OOP Exam - 11 December 2021

---
+++ Gym/Models/Athletes/Boxer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Athletes
+{
+    public class Boxer : Athlete
+    {
+        public Boxer(string fullName, string motivation, int numberOfMedals, int stamina) : base(fullName, motivation, numberOfMedals, stamina)
+        {
+        }
+
+        public override void Exercise()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
