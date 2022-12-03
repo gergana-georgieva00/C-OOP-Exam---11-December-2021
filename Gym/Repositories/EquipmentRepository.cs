30a1e4a45cb44d11d0a7163696dabe870a7abaad
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-03 16:31:55 +0200

    C# OOP Exam - 11 December 2021

---
+++ Gym/Repositories/EquipmentRepository.cs
@@ -0,0 +1,35 @@
+﻿using Gym.Models.Gyms.Contracts;
+using Gym.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Repositories
+{
+    public class EquipmentRepository : IRepository<IGym>
+    {
+        private List<IGym> models;
+
+        public EquipmentRepository()
+        {
+            this.models = new List<IGym>();
+        }
+
+        public IReadOnlyCollection<IGym> Models => this.models.AsReadOnly();
+
+        public void Add(IGym model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IGym FindByType(string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IGym model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
